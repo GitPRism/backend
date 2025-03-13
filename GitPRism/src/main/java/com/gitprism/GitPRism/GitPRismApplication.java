5793d50cd3f376edd038dc9257fd2b9037350b5c
SonSeokYoung <thdjemtlsssy318@gmail.com>
2025-03-14 03:52:53 +0900

    feat : spring 패키지 설정

---
+++ GitPRism/src/main/java/com/gitprism/GitPRism/GitPRismApplication.java
@@ -0,0 +1,12 @@
+package com.gitprism.GitPRism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPRismApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GitPRismApplication.class, args);
+	}
+
+}
