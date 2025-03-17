069fe0d5dd61b1b8454ce60fb483b820b6540b60
SonSeokYoung <thdjemtlsssy318@gmail.com>
2025-03-17 15:57:19 +0900

    feat : swagger 설정

--- GitPRism/src/main/java/com/gitprism/GitPRism/GitPRismApplication.java
+++ GitPRism/src/main/java/com/gitprism/GitPRism/GitPRismApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitPRismApplication {
 	public static void main(String[] args) {
+		// Spring Boot 실행
 		SpringApplication.run(GitPRismApplication.class, args);
 	}
-
 }
